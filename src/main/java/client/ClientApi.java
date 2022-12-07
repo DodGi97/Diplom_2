5391736062dba7210950c44c486ccc8c5a2284a0
DodGi97 <gudimivan86@gmail.com>
2022-12-07 23:48:10 +0600

    create first test

---
+++ src/main/java/client/ClientApi.java
@@ -0,0 +1,16 @@
+package client;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+import static constans.Url.BASE_URL;
+
+public class ClientApi {
+    protected RequestSpecification getBaseSpec() {
+        return new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .setBaseUri(BASE_URL)
+                .build();
+    }
+}
